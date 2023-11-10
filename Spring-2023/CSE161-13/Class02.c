a058192ff5da915935e398850edfef84915d5064
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2023-11-10 10:07:04 +0600

    Create Class02.c

---
+++ Spring-2023/CSE161-13/Class02.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int addNumber(){
+
+}
+
+int main(){
+    //const int x = 4;
+    //printf("%d\n", x);
+
+    char grade = 'A'; //character
+    char name[] = "Jack"; //String with double quotation
+    char myFriendName[] = {'J', 'o', 'n', '\0'}; // String with array
+    printf("%c\n", grade);
+    printf("%s\n", name);
+    printf("%s\n", myFriendName);
+    return 0;
+}
