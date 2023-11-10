3dda7db02d5c9d68ac13ad5a1136a5be3cbd22b8
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2023-11-10 10:04:40 +0600

    Create Class01.c

---
+++ Spring-2023/CSE162-15/Class01.c
@@ -0,0 +1,17 @@
+#include<stdio.h> //Preprocessor command
+
+int add(){
+    printf("Calling add function."); // Display some texts
+}
+
+//Program start from main function
+int main(){
+    /* This is our
+    first program in c */
+    printf("Hello, Tanvir!"); // Display some texts
+    return 0;
+}
+
+int substruct(){
+    printf("Calling substruct function.");
+}
