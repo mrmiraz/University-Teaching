c76c2d422a36197dd1688beaf03e5b1127b056fb
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2023-11-10 10:06:26 +0600

    Create Class01.c

---
+++ Spring-2023/CSE161-13/Class01.c
@@ -0,0 +1,16 @@
+#include<stdio.h> // preprocessor command
+void firstLine(){
+   printf("first function\n");
+}
+
+void secondLine(){
+   printf("second function\n");
+}
+
+int main(){
+    /* This is our
+    first program */
+    // Single line comment
+    firstLine();
+    printf("Hello, World!");
+}
