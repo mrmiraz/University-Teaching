e02b984af9118cacbea160a542280a4d32df68bc
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2023-11-10 10:01:22 +0600

    Create Class01.c

---
+++ Spring-2023/CSE262-13/Class01/Class01.c
@@ -0,0 +1,9 @@
+#include<stdio.h> //Preprocessor command
+
+//Program start from main function
+int main(){
+    /* This is our
+    first program in c */
+    printf("Hello, World!"); // Display some texts
+    return 0;
+}
