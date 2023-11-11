ad4ef7d49a222007935a88fb0ff2e51f15cc710c
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2023-11-11 16:17:25 +0600

    Create ClassTwo.c

---
+++ Spring-2023/CSE161-14/Class02/ClassTwo.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+/*
+void upperFunction(){
+    printf("Upper Function\n");
+}
+*/
+
+int main(){
+    /* Multiple
+    comments*/
+    //Single line comments
+    //This is my first program
+    //printf("Hello, World!\n"); // Display some texts
+    //upperFunction();
+    //lowerFunction();
+
+    //int myFavouriteNumber = 7;
+    /*
+    int myFavouriteNumber;
+    myFavouriteNumber = 7;
+    printf("%d\n", myFavouriteNumber);
+    */
+    // firstNumber is not same as FirstNumber
+    /*
+    int firstNumber = 1;
+    int FirstNumber = 2;
+    printf("%d\n", firstNumber);
+    printf("%d\n", FirstNumber);
+    int int = 1;
+    */
+
+    //Constant
+    //const int myFavouriteNumber = 7;
+    //myFavouriteNumber = 8;
+    //printf("%d", myFavouriteNumber);
+
+    //String
+    /*
+    //char myGrade = '1'; // This is a character not digit
+    char myGrade = 'A';
+    //char myName[] = {'N','a','s','i','m', '\0'}; // \0 = null
+    char myName[] = "Nasim";
+    printf("%c\n", myGrade);
+    printf("%s", myName);
+    */
+
+    // Operators
+
+    //Unary operator
+    /*
+    int a = 4;
+    int c;
+    c = a++; //increment by 1
+    printf("Value of a = %d\n", a);
+    printf("Value of c = %d\n", c);
+    int b = 5;
+    b--; // decrement by 1, b = b - 1;
+    printf("Value of b = %d\n", b);
+    */
+
+    //Binary Operators
+    /*
+    //Arithmetic Operators
+    int a = 7;
+    int b = 4;
+    int c = a % b;
+    printf("Arithmetic operation of a and b = %d", c);
+    */
+
+    //Relational Operators
+    /*
+    int a = 4;
+    int b = 5;
+    printf("%d", (a < b));
+    */
+
+    //Logical Operators
+
+    //Assignment Operators
+    int a = 4;
+    //a += 1;
+    a = a + 1;
+    printf("%d", a);
+    return 0;
+}
+
+/*
+void lowerFunction(){
+    printf("Lower Function");
+}
+*/
