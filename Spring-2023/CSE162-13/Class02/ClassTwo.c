28c95cdb2d68a96eea818b1ff62cb39da66de880
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2023-11-12 12:05:42 +0600

    Create ClassTwo.c

---
+++ Spring-2023/CSE162-13/Class02/ClassTwo.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main(){
+    //printf("Hello, world!\n");
+
+    //Token
+    //const int myFavoriteNumber = 7;
+    //myFavoriteNumber = 8;
+    //int int = 7; //keywords can not be used as identifiers
+    //printf("%d", myFavoriteNumber);
+
+    //String
+    /*
+    char grade = 'A';
+    printf("%c\n", grade);
+    //char myString[] = {'K','a','r','i','m','\0'}; //\0 null which indicates end of string
+    char myString[] = "Karim";
+    printf("%s", myString);
+    */
+
+    //Unary Operators
+    /*
+    int a = 5;
+    int c;
+    a++; // a = a + 1, incremental operator
+    c = a++;
+    printf("Value of a is %d\n", a);
+    printf("Value of c is %d\n", c);
+    int b = 4;
+    b--; // b = b - 1, decremental operator
+    printf("Value of b is %d\n", b);
+    */
+
+    //Binary Operator
+    //Arithmetic [ +, -, *, /, %]
+    /*
+    int a = 22;
+    int b = 4;
+    printf("%d", (a%b));
+    */
+
+    return 0;
+}
